198fd53add6025e5af347086a57d9081716a5a45
Vladimir Pak <TheRatW@gmail.com>
2015-08-15 19:57:02 +0300

    ConfigTreeBuilder - bug fix

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -19,7 +19,7 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('therat_branching');
+        $rootNode = $treeBuilder->root('the_rat_branching');
 
         /** @var ArrayNodeDefinition $rootNode */
         $rootNode
